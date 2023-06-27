087bb3b2a4971272483158e05e83cc79f3f1fc64
Karl Heinz Marbaise <kama@soebes.de>
2023-06-27 10:27:55 +0200

    Added JavaDoc for JEP 430.

--- src/test/java/com/soebes/jdk21/preview/TemplateTest.java
+++ src/test/java/com/soebes/jdk21/preview/TemplateTest.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test;
 import static java.lang.StringTemplate.STR;
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * JEP 430: String Templates (Preview)
+ *
+ * @see <a href="https://openjdk.org/jeps/430">JEP 430</a>
+ */
 class TemplateTest {
   @Test
   void name() {
