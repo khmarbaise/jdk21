b188ba8bdc8f7083c770953f228add37e422ed1b
Karl Heinz Marbaise <kama@soebes.de>
2023-08-10 19:37:40 +0200

    Improved main.

--- src/main/java/com/soebes/jdk21/Main.java
+++ src/main/java/com/soebes/jdk21/Main.java
@@ -1,8 +1,8 @@
 package com.soebes.jdk21;
 
-public class Main {
+interface Main {
 
-  public static void main(String[] args) {
+  static void main(String[] args) {
     System.out.println("Hello World.");
   }
 }
