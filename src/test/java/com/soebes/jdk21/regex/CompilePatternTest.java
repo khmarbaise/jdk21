9f3f76b9089c4fadb46ffbd85e42b1aeb02d53c4
Karl Heinz Marbaise <kama@soebes.de>
2023-10-03 14:47:21 +0200

    Added Regular expression.

---
+++ src/test/java/com/soebes/jdk21/regex/CompilePatternTest.java
@@ -0,0 +1,28 @@
+package com.soebes.jdk21.regex;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Pattern;
+
+class CompilePatternTest {
+
+  @Test
+  void patterns() {
+    System.out.println("PatternTest.patterns");
+    String line = "1;New York; 8 336 817";
+    var pattern = Pattern.compile("""
+        (?<index>\\d+);\
+        (?<city>[ a-zA-Z]+);\
+        (?<population>[ \\d]+)$""");
+
+    var matcher = pattern.matcher(line);
+    if (matcher.matches()) {
+      var index = matcher.group("index");
+      var city = matcher.group("city");
+      var population = matcher.group("population");
+      System.out.println("index = " + index);
+      System.out.println("city = " + city);
+      System.out.println("population = " + population);
+    }
+  }
+}
